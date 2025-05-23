c149c51e6071154550a4dae52e197e242da46462
Anjali8714 <anjalit9714@gmail.com>
2025-05-23 12:23:05 +0530

    fibonacci

--- Javascript/fibonacci.js
+++ Javascript/fibonacci.js
@@ -21,3 +21,21 @@ function fibonacci(n){
 }
 
 console.log(fibonacci(10))
+
+var fib = function(n) {
+
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+
+    let a = 0 , b = 1 ;
+
+   for(let i =2 ; i<=n ; i++) {
+ 
+    let t = a + b;
+    a = b;
+    b = t;
+}
+    return b
+}
+
+console.log(fib(2))
